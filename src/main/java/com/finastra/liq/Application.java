fda95b0a5302d2a9389e811845c85528c2a3f99b
Amitesh Ranjan Srivastava <amitesh430@gmail.com>
2022-09-27 04:07:44 +0000

    Initial commit

---
+++ src/main/java/com/finastra/liq/Application.java
@@ -0,0 +1,9 @@
+package com.finastra.liq;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
